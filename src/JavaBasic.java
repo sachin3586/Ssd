ab9bb5ea8beecac468e3e094f99f05580337a749
sachin3586 <sachindadhe55@gmail.com>
2024-07-11 23:26:10 +0530

    commited in  basic class by SSD

--- src/JavaBasic.java
+++ src/JavaBasic.java
@@ -2,5 +2,10 @@ public class JavaBasic {
     public static void main(String[] args) {
         System.out.println("This is SSD project");
         System.out.println("Hello Every one , How are you ????????????");
+
+        int a=10;
+        int b=50;
+        int c=a+b;
+        System.out.println(c);
     }
 }
