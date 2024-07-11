77ca1223b399fcd1ec19dde6d12b32ef1d2d9499
sachin3586 <sachindadhe55@gmail.com>
2024-07-11 23:19:11 +0530

    First commit by SACHIN DADHE

---
+++ src/JavaBasic.java
@@ -0,0 +1,6 @@
+public class JavaBasic {
+    public static void main(String[] args) {
+        System.out.println("This is SSD project");
+        System.out.println("Hello Every one , How are you ????????????");
+    }
+}
